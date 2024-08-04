e2e8b8f20da5ea523dacd4b8edc80491132fa9f3
GiveMePseudonyms <nathanhart@ntlworld.com>
2024-08-04 18:28:54 +0100

    config now reads from .JSON Please ask GiMePs for json file

--- Bot/config/config.go
+++ Bot/config/config.go
@@ -1,28 +1,85 @@
 package config
 
 import (
+	"encoding/json"
+	"os"
+
 	"github.com/bwmarrin/discordgo"
 )
+
+type Vars struct {
+	IsDev           bool
+	ReportedRunning bool
+	ServerName      string
+	BotToken        string
+	BullyTarget     string
+	FwidayCancelled bool
+
+	// discordgo (cannot be a constant, we update this on startup, needs to exist here for scope reasons)
+	Session *discordgo.Session
+
+	DB_NAME       string
+	DB_USER       string
+	DB_PASSWORD   string
+	DB_IP_ADDRESS string
+	DB_PORT       string
+}
 
 var (
 	// Runtime Environment ===========================================
 	IsDev           bool   = false
 	ReportedRunning bool   = false
 	ServerName      string = ""
-	BotToken        string = "MTI2OTYyOTY4NjIzMzgyNTI5MA.GmMv1T.vDmZJy5inKcVCxnx4ipe429o97Vg9PfaenSWDc"
+	BotToken        string = ""
 	BullyTarget     string = ""
 	FwidayCancelled bool   = false
 
 	// discordgo (cannot be a constant, we update this on startup, needs to exist here for scope reasons)
 	Session *discordgo.Session
+
+	// THESE DON'T CHANGE AT RUNTIME
+	// Database Config Settings
+	DB_NAME       string = ""
+	DB_USER       string = ""
+	DB_PASSWORD   string = ""
+	DB_IP_ADDRESS string = ""
+	DB_PORT       string = ""
 )
 
-const (
+func SetVars() {
+
+	fileText, err := os.ReadFile("Bot/config/config.JSON")
+
+	if err != nil {
+		panic("oops")
+		//using audit here causes import cycle
+	}
+
+	var vars Vars
+
+	jsonerr := json.Unmarshal([]byte(fileText), &vars)
+	if jsonerr != nil {
+		panic("oops")
+		//using audit here causes import cycle
+	}
+
+	// Runtime Environment ===========================================
+	IsDev = vars.IsDev
+	ReportedRunning = vars.ReportedRunning
+	ServerName = vars.ServerName
+	BotToken = vars.BotToken
+	BullyTarget = vars.BullyTarget
+	FwidayCancelled = vars.FwidayCancelled
+
+	// discordgo (cannot be a constant, we update this on startup, needs to exist here for scope reasons)
+	Session = vars.Session
+
+	// THESE DON'T CHANGE AT RUNTIME
 	// Database Config Settings
-	// TODO: Move this to be driven by command line args or env variable we set at bot startup in either dockerfile
-	DB_NAME       string = "BotDB"
-	DB_USER       string = "bot_access"
-	DB_PASSWORD   string = "AVNS_vGqsdK_m6vK2JLODhPI"
-	DB_IP_ADDRESS string = "db-mysql-nyc3-09006-do-user-16162189-0.c.db.ondigitalocean.com"
-	DB_PORT       string = "25060"
-)
+	DB_NAME = vars.ServerName
+	DB_USER = vars.DB_USER
+	DB_PASSWORD = vars.DB_PASSWORD
+	DB_IP_ADDRESS = vars.DB_IP_ADDRESS
+	DB_PORT = vars.DB_PORT
+
+}
